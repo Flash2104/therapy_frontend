da7158f52482784cdfaded5417a80b1b020a7b6a
k.khoruzhenko <khoruzhenko.work@gmail.com>
2021-09-19 22:34:46 +0300

    Add production build command

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  // apiEndpoint: "http://localhost:5000/api",
+	apiEndpoint: "https://api.kornevaya.ru/api",
+	//wsEndpoint: "ws://localhost:5000/notifications",
+	wsEndpoint: "wss://api.kornevaya.ru/notifications",
   production: true
 };
